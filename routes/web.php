5d9f7d51d4a115d5b045a7118bddbeed4bb76a56
Matt <19017841@brookes.ac.uk>
2023-11-16 21:54:09 +0000

    Modified welcome page and added API request to get cards

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Http;
+
+Route::get('/api/cards', function () {
+    try {
+        $response = Http::withOptions(['verify' => false])->get('https://higherorlower-api.netlify.app/json'); // disable SSL verification (do not do in prod)
+        return $response->json();
+    } catch (\Exception $e) {
+        console.log($e);
+    }
+});
