b94826e2bc218e7756ed9e9208467656f3a34a44
Matt <19017841@brookes.ac.uk>
2023-11-18 01:17:50 +0000

    Updated shuffleCards function to construct and reference card svg name

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,3 +10,26 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 }
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class YourController extends Controller
+{
+    public function shuffleCards(Request $request)
+    {
+        // Retrieve the JSON array from the query string
+        $cardsJson = $request->query('cardsJson');
+
+        // If a JSON array is provided, decode and shuffle it
+        if (!empty($cardsArray)) {
+            $data = json_decode($jsonArray, true);
+            shuffle($data);
+
+            return response()->json(['shuffledArray' => $data]);
+        }
+
+        return;
+    }
+}
